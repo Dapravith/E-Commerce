8dd555acf9da644cf52525c7fef87dafcdb1d9fe
ROTHA DAPRAVITH <90898700+Dapravith@users.noreply.github.com>
2023-03-21 07:57:36 +0700

    e-commerce

---
+++ src/views/Checkout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+    <h1>This is an checkout page</h1>
+    <br />
+    <button @click="$emit('cancel')">Cancel</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="home">
+    <h1>This is a home page</h1>
+  </div>
+</template>
